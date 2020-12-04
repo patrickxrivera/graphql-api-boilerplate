db8e6925e070a1c9fd6bba432d7f14257cfa3b11
pxr13 <patrick.x.rivera@gmail.com>
2020-12-03 19:28:07 -0500

    Use correct PORT

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const prisma = require('./setup/models')
 const { permissions } = require('./services/permissions')
 const types = require('./types')
 
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 const server = new ApolloServer({
   schema: applyMiddleware(
